9f12eb9cfccf5c59eb8c24c2f799d6da52a4cdc4
unknown <zlovred12@gmail.com>
2020-12-28 20:50:04 +0300

    gui

--- src/main/java/GUI/CalcGUI.java
+++ src/main/java/GUI/CalcGUI.java
@@ -10,9 +10,9 @@ public class CalcGUI extends JFrame implements ActionListener {
     JFrame frame;
     JTextField textField;
     JButton[] digitButtons = new JButton[10];
-    JButton[] functionButtons = new JButton[8];
+    JButton[] functionButtons = new JButton[9];
     JButton addButton,subButton,mulButton,divButton;
-    JButton decButton,equButton,delButton, clrButton;
+    JButton decButton,equButton,delButton, clrButton, negButton;
     JPanel panel;
 
     Font myFont = new Font("Ink Free", Font.BOLD, 30);
@@ -39,6 +39,7 @@ public class CalcGUI extends JFrame implements ActionListener {
         equButton = new JButton("=");
         delButton = new JButton("Delete");
         clrButton = new JButton("Clear");
+        negButton = new JButton("(-)");
 
         functionButtons[0] = addButton;
         functionButtons[1] = subButton;
@@ -48,8 +49,9 @@ public class CalcGUI extends JFrame implements ActionListener {
         functionButtons[5] = equButton;
         functionButtons[6] = delButton;
         functionButtons[7] = clrButton;
+        functionButtons[8] = negButton;
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 9; i++) {
             functionButtons[i].addActionListener(this);
             functionButtons[i].setFont(myFont);
             functionButtons[i].setFocusable(false);
@@ -62,8 +64,9 @@ public class CalcGUI extends JFrame implements ActionListener {
             digitButtons[i].setFocusable(false);
         }
 
-        delButton.setBounds(50, 430, 145, 50);
-        clrButton.setBounds(205, 430, 145, 50);
+        negButton.setBounds(50, 430, 100, 50);
+        delButton.setBounds(150, 430, 100, 50);
+        clrButton.setBounds(250, 430, 100, 50);
 
         panel = new JPanel();
         panel.setBounds(50, 100, 300, 300);
@@ -86,8 +89,8 @@ public class CalcGUI extends JFrame implements ActionListener {
         panel.add(equButton);
         panel.add(divButton);
 
-
         frame.add(panel);
+        frame.add(negButton);
         frame.add(delButton);
         frame.add(clrButton);
         frame.add(textField);
@@ -128,7 +131,21 @@ public class CalcGUI extends JFrame implements ActionListener {
         }
         if (e.getSource()==equButton){
             textField.setText(textField.getText().concat(String.valueOf(equButton.getText())));
-
+        }
+        if (e.getSource()==clrButton){
+            textField.setText("");
+        }
+        if (e.getSource()==delButton){
+            String temp = textField.getText();
+            textField.setText("");
+            for (int i = 0; i <temp.length() - 1 ; i++) {
+                textField.setText(textField.getText()+temp.charAt(i));
+            }
+        }
+        if (e.getSource()==negButton){
+            double temp = Double.parseDouble(textField.getText());
+            temp*=-1;
+            textField.setText(String.valueOf(temp));
         }
     }
 }
