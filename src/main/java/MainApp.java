1420e9bd5953148e5aef9fe0c07290bb5c4afdeb
unknown <zlovred12@gmail.com>
2020-12-28 19:11:31 +0300

    calculator

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,11 @@
+import Logic.*;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        CalcLogic calcLogic = new CalcLogic("1+1");
+    }
+}
