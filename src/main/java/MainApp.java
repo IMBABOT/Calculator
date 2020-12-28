cef29b759b405815a64fb6eda3eac6311d91cd4d
unknown <zlovred12@gmail.com>
2020-12-28 19:13:36 +0300

    calculator

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -6,6 +6,7 @@ import java.util.Stack;
 public class MainApp {
 
     public static void main(String[] args) {
-        CalcLogic calcLogic = new CalcLogic("1+1");
+        CalcLogic calcLogic = new CalcLogic("5855*654555+(55478+56985)");
+        CalcLogic calcLogic1 = new CalcLogic("1+1");
     }
 }
